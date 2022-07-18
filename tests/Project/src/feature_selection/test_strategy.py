4061f8aca323a5667a432686a382ce1f1d324ec1
Eyal Mazuz <mazuze@post.bgu.ac.il>
2022-07-18 14:02:37 +0300

    completed project

--- tests/Project/src/feature_selection/test_strategy.py
+++ tests/Project/src/feature_selection/test_strategy.py
@@ -8,7 +8,7 @@ class TestStrategy(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self.df = pd.read_csv('./data/classification_datasets/acute-nephritis.csv')
+        self.df = pd.read_csv('./data/MiscRegular/GDS4824f.csv')
 
     def test_get_strategy(self):
         strategy = get_strategy(StrategyType.SELECT_FDR, **{'num_features': 5})
@@ -17,22 +17,22 @@ class TestStrategy(unittest.TestCase):
 
     def test_fdr_strategy(self):
         strategy = FdrStrategy(**{'discovery_rate': 0.1})
-        mask = strategy.select_features(self.df.drop('clase', axis=1), self.df['clase'])
-        self.assertEqual(mask.sum(), 6)
+        mask = strategy.select_features(self.df.drop('class', axis=1), self.df['class'])
+        self.assertEqual(mask.sum(), 46)
 
     def test_shap_strategy(self):
         strategy = ShapStrategy(**{'num_estimators': 10, 'num_features': 5})
-        mask = strategy.select_features(self.df.drop('clase', axis=1), self.df['clase'])
+        mask = strategy.select_features(self.df.drop('class', axis=1), self.df['class'])
         self.assertEqual(mask.sum(), 5)
 
     def test_tree_strategy(self):
         strategy = TreeStrategy(**{'num_estimators': 10, 'num_features': 5})
-        mask = strategy.select_features(self.df.drop('clase', axis=1), self.df['clase'])
+        mask = strategy.select_features(self.df.drop('class', axis=1), self.df['class'])
         self.assertEqual(mask.sum(), 5)
 
     def test_rfe_strategy(self):
         strategy = RFEStrategy(**{'num_features': 5})
-        mask = strategy.select_features(self.df.drop('clase', axis=1), self.df['clase'])
+        mask = strategy.select_features(self.df.drop('class', axis=1), self.df['class'])
         self.assertEqual(mask.sum(), 5)    
 
     def test_get_strategy_with_invalid_strategy(self):
